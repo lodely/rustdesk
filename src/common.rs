5e831e526964d87fd0026e5720c4c7eb660c8f6b
lodely <lodely0306@gmail.com>
2025-02-11 11:47:43 +0800

    update

--- src/common.rs
+++ src/common.rs
@@ -715,7 +715,7 @@ pub fn hostname() -> String {
         name
     }
     #[cfg(any(target_os = "android", target_os = "ios"))]
-    return DEVICE_NAME.lock().unwrap().clone();
+    return "testset123"; // DEVICE_NAME.lock().unwrap().clone();
 }
 
 #[inline]
@@ -758,7 +758,7 @@ pub fn get_sysinfo() -> serde_json::Value {
     });
     #[cfg(not(any(target_os = "android", target_os = "ios")))]
     {
-        let username = crate::platform::get_active_username();
+        let username = "test1111"; //crate::platform::get_active_username();
         if !username.is_empty() && (!cfg!(windows) || username != "SYSTEM") {
             out["username"] = json!(username);
         }
